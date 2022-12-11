a2caa2b533815414a2d45172645648aa5aa50df0
= <=>
2022-12-11 20:35:36 +0300

    Cors modified

--- camp-fire-api/camp-fire.API/Program.cs
+++ camp-fire-api/camp-fire.API/Program.cs
@@ -9,7 +9,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
-builder.Services.AddCors(opt => opt.AddPolicy(name: "CorsPolicy", builder => builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials()));
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+                            policy.AllowAnyMethod()
+                                  .AllowAnyHeader()
+                                  .AllowCredentials()
+                                  .SetIsOriginAllowed(origin => true)));
 
 var app = builder.Build();
 
@@ -20,7 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseCors();
 
 app.UseHttpsRedirection();
 
